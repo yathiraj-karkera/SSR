0f4d59c9e17d3b47972da7e77145bc37f46dbc5d
Yathiraj Karkera <yathirajKarkera@juegostudio.net>
2020-06-11 20:05:35 +0530

    removed duplicate files

--- proj.ios_mac/SuperSwitchRun/proj.android/app/libs/tweet-composer/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Volumes/extend/jenkins/sdkbox/release/plugins/share/proj.android/libs/tweet-composer/bin/AndroidManifest.xml : \
-/Volumes/extend/jenkins/sdkbox/release/plugins/share/proj.android/libs/tweet-composer/AndroidManifest.xml \
--- proj.ios_mac/SuperSwitchRun/proj.android/app/libs/twitter-core/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Volumes/extend/jenkins/sdkbox/release/plugins/share/proj.android/libs/twitter-core/bin/AndroidManifest.xml : \
-/Volumes/extend/jenkins/sdkbox/release/plugins/share/proj.android/libs/twitter-core/AndroidManifest.xml \
